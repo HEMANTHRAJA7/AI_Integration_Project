9407ec28305a18b9b36a623d5513dd2387275cc0
Hemanth Raja <hemanthraja2004@gmail.com>
2025-05-22 09:27:39 +0530

    convex problem rectified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,16 +26,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ConvexClerkProvider>
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <Navbar />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </ConvexClerkProvider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navbar />
+
+        {/* GRID BACKGROUND */}
+        <div className="fixed inset-0 -z-1">
+          <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+          <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+        </div>
+
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
